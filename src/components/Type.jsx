543aece905531e81378f7c95a73a4b8f7ee63746
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-12-10 11:59:23 +0545

    removed extra text

--- src/components/Type.jsx
+++ src/components/Type.jsx
@@ -3,9 +3,7 @@ import { ReactTyped } from "react-typed";
 
 const Type = () => (
   <div className="space-y-2">
-    <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-foreground">
-      Namaste! I am Sarthak Ghimire.
-    </h1>
+
     <ReactTyped
       strings={[
         "I am a student...",
